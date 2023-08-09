42fdab9e8d56a69b987970f1091054ce853dacc1
angelbencosme2022 <109389381+angelbencosme2022@users.noreply.github.com>
2023-08-08 23:21:42 -0400

    Add files via upload

---
+++ stacks.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+int main(){
+std::stack<int> stack;
+
+for(int i = 0; i < 10; i++){
+    stack.push(i);
+}
+
+while(!stack.empty()){
+    std::cout << stack.top() << std::endl;
+    stack.pop();
+}
+std::cout << (stack.size()) << std::endl;
+return 0;
+}
